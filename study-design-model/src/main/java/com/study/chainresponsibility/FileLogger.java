5d56905ccbe8add2de4af11db0b2b537d6e70940
zhangjie <jie.zhang51@caocaoglobal.com>
2023-03-01 10:50:11 +0800

    设计模式

---
+++ study-design-model/src/main/java/com/study/chainresponsibility/FileLogger.java
@@ -0,0 +1,14 @@
+package com.study.chainresponsibility;
+
+public class FileLogger extends AbstractLogger{
+
+    public FileLogger(int level){
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("File::Logger: " + message);
+    }
+
+}
--- study-juc/src/main/java/com/study/juc/third/CyclicBarrier/T07_TestCyclicBarrier.java
+++ study-juc/src/main/java/com/study/juc/third/CyclicBarrier/T07_TestCyclicBarrier.java
@@ -5,7 +5,10 @@ import java.util.concurrent.CyclicBarrier;
 
 /**
  * CyclicBarrier，意思是循环栅栏，这有一个栅栏，什么时候人满了就把栅栏推倒
- *  对于一个复杂操作，需要访问数据库，需要访问网络，需要访问文件，有一种方式是顺序执行
+ *  对于一个复杂操作，需要访问数据库，需要访问网络，需要访问文件，有一种方式是顺序执行，挨个的都执行完，效率非常低
+ *  这是一种方式，还有一种可能性就是并发执行，原来是1、2、3顺序执行，并发执行是不同线程去执行不同的操作，有的线程
+ *  去数据库找，有的线程去网络访问，有的线程去读文件，必须是这三个线程全部到位了我才能去进行，这个时候我们就可以用
+ *  CyclicBarrier
  */
 public class T07_TestCyclicBarrier {
     public static void main(String[] args) {
