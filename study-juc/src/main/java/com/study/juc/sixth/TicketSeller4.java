731a4bbe70c97e615e76414b2161e7dc14038752
zhangjie <jie.zhang51@caocaoglobal.com>
2023-04-12 13:35:55 +0800

    TicketSeller

---
+++ study-juc/src/main/java/com/study/juc/sixth/TicketSeller4.java
@@ -0,0 +1,25 @@
+package com.study.juc.sixth;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class TicketSeller4 {
+
+    static Queue<String> tickets = new ConcurrentLinkedQueue<>();
+
+    static {
+        for (int i = 0; i < 1000; i++) tickets.add("票编号" + i);
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                while (true) {
+                    String s =  tickets.poll();
+                    if (s == null) break;
+                    else System.out.println("销售了--" + s);
+                }
+            }).start();
+        }
+    }
+}
